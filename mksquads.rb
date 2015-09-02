f8733cd5afd403152b0c89fdabe2483aff6577aa
Gerald Bauer <gerald.bauer@gmail.com>
2015-09-02 10:19:04 +0200

    up

--- mksquads.rb
+++ mksquads.rb
@@ -9,61 +9,20 @@ require 'textutils'
 
 ## our own code
 require './countries'
+require './clubs'
 
-
-
-################################
-# note: for now "hard-coded" for de-deutschland/2015-16
-#   make more "generic" as more import data gets used
-
-
-text = File.read_utf8( './de-deutschland/2015-16/1-bundesliga-squads.json' )
-
-clubs = JSON.parse( text )
-
-## pp h
-
-CLUB_KEY = {
-  '1. FC Köln'           => 'koeln',
-  '1. FSV Mainz 05'      => 'mainz',
-  'Bayer 04 Leverkusen'  => 'leverkusen',
-  'Bayern München'       => 'bayern',
-  'Bor. Mönchengladbach' => 'mgladbach',
-  'Borussia Dortmund'    => 'dortmund',
-  'Eintracht Frankfurt'  => 'frankfurt',
-  'FC Augsburg'          => 'augsburg',
-  'FC Ingolstadt 04'     => 'ingolstadt',
-  'FC Schalke 04'        => 'schalke',
-  'Hamburger SV'         => 'hamburg',   ## change to hamburg!!
-  'Hannover 96'          => 'hannover',
-  'Hertha BSC'           => 'herthabsc',
-  'SV Darmstadt 98'      => 'darmstadt',
-  'TSG Hoffenheim'       => 'hoffenheim',
-  'VfB Stuttgart'        => 'stuttgart',
-  'VfL Wolfsburg'        => 'wolfsburg',
-  'Werder Bremen'        => 'bremen',
-  'VfL Bochum'           => 'bochum'
-  'SC Freiburg'          => 'freiburg'
-  '1. FC Heidenheim'     => 'heidenheim'
-  'FC St. Pauli'         => 'pauli'
-   'RasenBallsport Leipzig' => 'leipzig'
-  '1. FC Kaiserslautern' => 'kaiserslautern'
-  'SV Sandhausen'        => 'sandhausen'
-  'Eintracht Braunschweig' => 'braunschweig'
-  'FSV Frankfurt'        => 'fsvfrankfurt'
-  '1. FC Nürnberg'       => 'nuernberg'
-  'Arminia Bielefeld'    => 'bielefeld'
-  'Karlsruher SC'        => 'karlsruhe'
-  'SpVgg Greuther Fürth' => 'fuerth'
-  '1. FC Union Berlin'   => 'unionberlin'
-  'SC Paderborn 07'      => 'paderborn'
-  '1860 München'         => '1860muenchen'
-  'MSV Duisburg'         => 'duisburg'
-  'Fortuna Düsseldorf'   => 'duesseldorf'
-}
 
 
 MISSING_COUNTRIES = {}
+
+
+def calc_age( today, birthdate )
+   age = today.year - birthdate.year
+
+   age -= 1   if today.month <  birthdate.month
+   age -= 1   if today.month == birthdate.month && today.day < birthdate.day
+   age
+end
 
 
 def gen_squad( club, h )
@@ -74,12 +33,18 @@ def gen_squad( club, h )
 
   last_position = ''
   h.each do |(k,v)|
-    num       = v['number']
-    birthdate = v['birthdate']
-    name      = k
-    position  = v['position']
-    country   = v['country']
-    since     = v['since']
+    num           = v['number']
+    birthdate_str = v['birthdate']
+    name          = k
+    position      = v['position']
+    country       = v['country']
+    since         = v['since']
+
+    birthdate = Date.strptime( birthdate_str, '%d.%m.%Y' )  ## e.g. assumes 01.01.1982
+    
+    today     = Date.new( 2015, 8, 31 )  ## use for age calc (start of season)
+    age =  calc_age( today, birthdate )
+    
 
     if num == 'xx'
       num = ''   # convert unknown number eg. xx to empty string
@@ -112,7 +77,7 @@ def gen_squad( club, h )
 
     buf << "%s  " % position
     buf << "%s-       " % since
-    buf << "# %s"  % birthdate
+    buf << "#  %11s (%s)"  % [birthdate.strftime( '%-d %b %Y' ), age]   ## e.g. reformat to 1 Jan 1984
     buf << "\n"
     
     last_position = position
@@ -130,48 +95,63 @@ SORT_POSITION = {
 }
 
 
-###
-## missing nummber is xx  -- what to do? set to ? or empty string ???
 
-## out_root = './build/de-deutschland'
-out_root = '../../openfootball/de-deutschland/2015-16/squads'
+def make_squads( clubs, opts={} )
 
-clubs.each_with_index do |(k,v), i|
-  club       = k
-  club_key   = CLUB_KEY[ k ]
-  players    = v
+  out_dir = opts[:out_dir]   ## e.g. './build/de-deutschland'
 
-  players = players.sort do |l,r|
-    ## note gets us an array e.g.
-    ##   ["Osako, Yuya", {"birthdate"=>"18.05.1990", "country"=>"Japan", "number"=>"13", "position"=>"FW"}]
-    ##  e.g. l[0] is the key; l[1] is the value e.g. hash
-    lpos = l[1]['position']   
-    rpos = r[1]['position']
+  clubs.each_with_index do |(k,v), i|
+    club       = k
+    club_key   = CLUB_KEY[ k ]
+    players    = v
 
-    value =  SORT_POSITION[lpos] <=> SORT_POSITION[rpos]
-    value =  l[1]['number'].to_i <=> r[1]['number'].to_i    if value == 0
-    value
-  end
-  players = players.to_h  # "convert" it back (after sorting) into an hash
-  ##  pp players
+    players = players.sort do |l,r|
+      ## note gets us an array e.g.
+      ##   ["Osako, Yuya", {"birthdate"=>"18.05.1990", "country"=>"Japan", "number"=>"13", "position"=>"FW"}]
+      ##  e.g. l[0] is the key; l[1] is the value e.g. hash
+      lpos = l[1]['position']   
+      rpos = r[1]['position']
+
+      value =  SORT_POSITION[lpos] <=> SORT_POSITION[rpos]
+      value =  l[1]['number'].to_i <=> r[1]['number'].to_i    if value == 0
+      value
+    end
+    players = players.to_h  # "convert" it back (after sorting) into an hash
+    ##  pp players
   
-  puts "*** #{club}"
+    puts "*** #{club}"
     
-  out_path = "#{out_root}/#{club_key}.txt"
-  out_dir  = File.dirname( out_path )
+    out_path = "#{out_dir}/#{club_key}.txt"
 
-  puts "#{club_key} - #{out_path}"
-  txt = gen_squad( club, players )
-  puts txt
+    puts "#{club_key} - #{out_path}"
+    txt = gen_squad( club, players )
+    puts txt
 
-  ## make sure out_path exists
-  ## make sure dest folder exists
-  FileUtils.mkdir_p( out_dir ) unless Dir.exists?( out_dir )
+    ## make sure out_path exists
+    ## make sure dest folder exists
+    FileUtils.mkdir_p( out_dir ) unless Dir.exists?( out_dir )
   
-  File.open( out_path, 'w' ) do |f|
-    f.write txt
+    File.open( out_path, 'w' ) do |f|
+      f.write txt
+    end
   end
-end
+end  # make_squads
+
+
+
+################################
+# let's go
+
+de1 = JSON.parse( File.read_utf8( './de-deutschland/2015-16/1-bundesliga-squads.json' ))
+de2 = JSON.parse( File.read_utf8( './de-deutschland/2015-16/2-bundesliga-squads.json' ))
+
+## for debugging
+## make_squads( de1, out_dir: './build/de-deutschland' )
+## make_squads( de2, out_dir: './build/de-deutschland' )
+
+make_squads( de1, out_dir: '../../openfootball/de-deutschland/2015-16/squads' )
+make_squads( de2, out_dir: '../../openfootball/de-deutschland/2015-16/squads' )
+
 
 pp MISSING_COUNTRIES
 
